1936729602643d180c07e85ea6320603a94fc1fd
brian <mr.brianhobo@gmail.com>
2020-02-12 02:53:05 -0800

    implement CRUD operations in store

--- storage/memory/store.go
+++ storage/memory/store.go
@@ -14,6 +14,7 @@ type Store struct {
 	mut                  sync.RWMutex
 	projects             map[uuid.UUID]model.Project
 	transactionTemplates map[uuid.UUID]model.TransactionTemplate
+	scriptTemplates      map[uuid.UUID]model.ScriptTemplate
 }
 
 func NewStore() *Store {
@@ -21,6 +22,7 @@ func NewStore() *Store {
 		mut:                  sync.RWMutex{},
 		projects:             make(map[uuid.UUID]model.Project),
 		transactionTemplates: make(map[uuid.UUID]model.TransactionTemplate),
+		scriptTemplates:      make(map[uuid.UUID]model.ScriptTemplate),
 	}
 }
 
@@ -143,6 +145,86 @@ func (s *Store) DeleteTransactionTemplate(id uuid.UUID) error {
 	}
 
 	delete(s.transactionTemplates, id)
+
+	return nil
+}
+
+func (s *Store) InsertScriptTemplate(tpl *model.ScriptTemplate) error {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
+	var tpls []*model.ScriptTemplate
+	err := s.getScriptTemplatesForProject(tpl.ProjectID, &tpls)
+	if err != nil {
+		return err
+	}
+
+	count := len(tpls)
+
+	// set index to one after last
+	tpl.Index = count
+
+	s.scriptTemplates[tpl.ID] = *tpl
+
+	return nil
+}
+
+func (s *Store) UpdateScriptTemplate(
+	input model.UpdateScriptTemplate,
+	tpl *model.ScriptTemplate,
+) error {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
+	t, ok := s.scriptTemplates[input.ID]
+	if !ok {
+		return storage.ErrNotFound
+	}
+
+	t.Script = input.Script
+
+	s.scriptTemplates[input.ID] = t
+
+	*tpl = t
+
+	return nil
+}
+
+func (s *Store) GetScriptTemplate(id uuid.UUID, tpl *model.ScriptTemplate) error {
+	s.mut.RLock()
+	defer s.mut.RUnlock()
+
+	t, ok := s.scriptTemplates[id]
+	if !ok {
+		return storage.ErrNotFound
+	}
+
+	*tpl = t
+
+	return nil
+}
+
+func (s *Store) GetScriptTemplatesForProject(projectID uuid.UUID, tpls *[]*model.ScriptTemplate) error {
+	s.mut.RLock()
+	defer s.mut.RUnlock()
+
+	return s.getScriptTemplatesForProject(projectID, tpls)
+}
+
+func (s *Store) getScriptTemplatesForProject(projectID uuid.UUID, tpls *[]*model.ScriptTemplate) error {
+	res := make([]*model.ScriptTemplate, 0)
+
+	for _, tpl := range s.scriptTemplates {
+		if tpl.ProjectID == projectID {
+			t := tpl
+			res = append(res, &t)
+		}
+	}
+
+	// sort results by index
+	sort.Slice(res, func(i, j int) bool { return res[i].Index < res[j].Index })
+
+	*tpls = res
 
 	return nil
 }
