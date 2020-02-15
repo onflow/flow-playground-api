0da75ee8e894475505e8defc5787fc5c60a46a04
brian <mr.brianhobo@gmail.com>
2020-02-14 16:16:23 -0800

    add unit tests for script templates

--- playground_test.go
+++ playground_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/dapperlabs/flow-playground-api"
+	playground "github.com/dapperlabs/flow-playground-api"
 	"github.com/dapperlabs/flow-playground-api/storage/memory"
 )
 
@@ -83,6 +83,45 @@ mutation($templateId: UUID!, $index: Int!) {
 const MutationDeleteTransactionTemplate = `
 mutation($templateId: UUID!) {
   deleteTransactionTemplate(id: $templateId)
+}
+`
+
+const MutationCreateScriptTemplate = `
+mutation($projectId: UUID!, $script: String!) {
+  createScriptTemplate(input: { projectId: $projectId, script: $script }) {
+    id
+	script
+	index
+  }
+}
+`
+
+const QueryGetScriptTemplate = `
+query($templateId: UUID!) {
+  scriptTemplate(id: $templateId) {
+    id
+    script
+  }
+}
+`
+
+const MutationUpdateScriptTemplateScript = `
+mutation($templateId: UUID!, $script: String!) {
+  updateScriptTemplate(input: { id: $templateId, script: $script }) {
+    id
+	script
+    index
+  }
+}
+`
+
+const MutationUpdateScriptTemplateIndex = `
+mutation($templateId: UUID!, $index: Int!) {
+  updateScriptTemplate(input: { id: $templateId, index: $index }) {
+    id
+	script
+    index
+  }
 }
 `
 
@@ -382,6 +421,176 @@ func TestTransactionTemplates(t *testing.T) {
 		c.MustPost(MutationDeleteTransactionTemplate, &resp, client.Var("templateId", templateID))
 
 		assert.Equal(t, templateID, resp.DeleteTransactionTemplate)
+	})
+}
+
+func TestScriptTemplates(t *testing.T) {
+	t.Run("Create script template", func(t *testing.T) {
+		c := newClient()
+
+		projectID := createProject(c)
+
+		var resp struct {
+			CreateScriptTemplate struct {
+				ID     string
+				Script string
+				Index  int
+			}
+		}
+
+		c.MustPost(
+			MutationCreateScriptTemplate,
+			&resp,
+			client.Var("projectId", projectID),
+			client.Var("script", "foo"),
+		)
+
+		assert.NotEmpty(t, resp.CreateScriptTemplate.ID)
+		assert.Equal(t, "foo", resp.CreateScriptTemplate.Script)
+	})
+
+	t.Run("Get script template", func(t *testing.T) {
+		c := newClient()
+
+		projectID := createProject(c)
+
+		var respA struct {
+			CreateScriptTemplate struct {
+				ID     string
+				Script string
+				Index  int
+			}
+		}
+
+		c.MustPost(
+			MutationCreateScriptTemplate,
+			&respA,
+			client.Var("projectId", projectID),
+			client.Var("script", "foo"),
+		)
+
+		var respB struct {
+			ScriptTemplate struct {
+				ID     string
+				Script string
+				Index  int
+			}
+		}
+
+		c.MustPost(
+			QueryGetScriptTemplate,
+			&respB,
+			client.Var("templateId", respA.CreateScriptTemplate.ID),
+		)
+
+		assert.Equal(t, respA.CreateScriptTemplate.ID, respB.ScriptTemplate.ID)
+		assert.Equal(t, respA.CreateScriptTemplate.Script, respB.ScriptTemplate.Script)
+	})
+
+	t.Run("Get non-existent script template", func(t *testing.T) {
+		c := newClient()
+
+		var resp struct {
+			ScriptTemplate struct {
+				ID     string
+				Script string
+				Index  int
+			}
+		}
+
+		badID := uuid.New().String()
+
+		err := c.Post(
+			QueryGetScriptTemplate,
+			&resp,
+			client.Var("templateId", badID),
+		)
+
+		assert.Error(t, err)
+	})
+
+	t.Run("Update script template", func(t *testing.T) {
+		c := newClient()
+
+		projectID := createProject(c)
+
+		var respA struct {
+			CreateScriptTemplate struct {
+				ID     string
+				Script string
+				Index  int
+			}
+		}
+
+		c.MustPost(
+			MutationCreateScriptTemplate,
+			&respA,
+			client.Var("projectId", projectID),
+			client.Var("script", "foo"),
+		)
+
+		templateID := respA.CreateScriptTemplate.ID
+
+		var respB struct {
+			UpdateScriptTemplate struct {
+				ID     string
+				Script string
+				Index  int
+			}
+		}
+
+		c.MustPost(
+			MutationUpdateScriptTemplateScript,
+			&respB,
+			client.Var("templateId", templateID),
+			client.Var("script", "bar"),
+		)
+
+		assert.Equal(t, respA.CreateScriptTemplate.ID, respB.UpdateScriptTemplate.ID)
+		assert.Equal(t, respA.CreateScriptTemplate.Index, respB.UpdateScriptTemplate.Index)
+		assert.Equal(t, "bar", respB.UpdateScriptTemplate.Script)
+
+		var respC struct {
+			UpdateScriptTemplate struct {
+				ID     string
+				Script string
+				Index  int
+			}
+		}
+
+		c.MustPost(
+			MutationUpdateScriptTemplateIndex,
+			&respC,
+			client.Var("templateId", templateID),
+			client.Var("index", 1),
+		)
+
+		assert.Equal(t, respA.CreateScriptTemplate.ID, respC.UpdateScriptTemplate.ID)
+		assert.Equal(t, 1, respC.UpdateScriptTemplate.Index)
+		assert.Equal(t, respB.UpdateScriptTemplate.Script, respC.UpdateScriptTemplate.Script)
+	})
+
+	t.Run("Update non-existent script template", func(t *testing.T) {
+		c := newClient()
+
+		var resp struct {
+			ScriptTemplate struct {
+				ID     string
+				Script string
+				Index  int
+			}
+		}
+
+		badID := uuid.New().String()
+
+		err := c.Post(
+			MutationUpdateScriptTemplateScript,
+			&resp,
+			client.Var("templateId", badID),
+			client.Var("script", "bar"),
+		)
+
+		assert.Error(t, err)
 	})
 }
 
