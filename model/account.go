603c6f5991be90e9613b9f26eeedd2be83142a86
Kay-Zee <kan@axiomzen.co>
2020-03-01 22:14:52 -0800

    Pass all existing playground tests with datastore

--- model/account.go
+++ model/account.go
@@ -3,6 +3,7 @@ package model
 import (
 	"cloud.google.com/go/datastore"
 	"github.com/google/uuid"
+	"github.com/pkg/errors"
 )
 
 type Account struct {
@@ -16,4 +17,60 @@ type Account struct {
 
 func (a *Account) NameKey() *datastore.Key {
 	return datastore.NameKey("Account", a.ID.String(), nil)
+}
+
+func (a *Account) Load(ps []datastore.Property) error {
+	tmp := struct {
+		ID           string
+		ProjectID    string
+		Index        int
+		Address      []byte
+		DraftCode    string
+		DeployedCode string
+	}{}
+
+	if err := datastore.LoadStruct(&tmp, ps); err != nil {
+		return err
+	}
+
+	if err := a.ID.UnmarshalText([]byte(tmp.ID)); err != nil {
+		return errors.Wrap(err, "failed to decode UUID")
+	}
+	if err := a.ProjectID.UnmarshalText([]byte(tmp.ProjectID)); err != nil {
+		return errors.Wrap(err, "failed to decode UUID")
+	}
+	a.Index = tmp.Index
+	copy(a.Address[:], tmp.Address[:])
+	a.DraftCode = tmp.DraftCode
+	a.DeployedCode = tmp.DeployedCode
+	return nil
+}
+
+func (a *Account) Save() ([]datastore.Property, error) {
+	return []datastore.Property{
+		{
+			Name:  "ID",
+			Value: a.ID.String(),
+		},
+		{
+			Name:  "ProjectID",
+			Value: a.ProjectID.String(),
+		},
+		{
+			Name:  "Index",
+			Value: a.Index,
+		},
+		{
+			Name:  "Address",
+			Value: a.Address[:],
+		},
+		{
+			Name:  "DraftCode",
+			Value: a.DraftCode,
+		},
+		{
+			Name:  "DeployedCode",
+			Value: a.DeployedCode,
+		},
+	}, nil
 }
--- model/script.go
+++ model/script.go
@@ -3,6 +3,7 @@ package model
 import (
 	"cloud.google.com/go/datastore"
 	"github.com/google/uuid"
+	"github.com/pkg/errors"
 )
 
 type ScriptTemplate struct {
@@ -14,6 +15,51 @@ type ScriptTemplate struct {
 
 func (s *ScriptTemplate) NameKey() *datastore.Key {
 	return datastore.NameKey("ScriptTemplate", s.ID.String(), nil)
+}
+
+func (s *ScriptTemplate) Load(ps []datastore.Property) error {
+	tmp := struct {
+		ID        string
+		ProjectID string
+		Index     int
+		Script    string
+	}{}
+
+	if err := datastore.LoadStruct(&tmp, ps); err != nil {
+		return err
+	}
+
+	if err := s.ID.UnmarshalText([]byte(tmp.ID)); err != nil {
+		return errors.Wrap(err, "failed to decode UUID")
+	}
+	if err := s.ProjectID.UnmarshalText([]byte(tmp.ProjectID)); err != nil {
+		return errors.Wrap(err, "failed to decode UUID")
+	}
+	s.Index = tmp.Index
+	s.Script = tmp.Script
+	return nil
+}
+
+func (s *ScriptTemplate) Save() ([]datastore.Property, error) {
+	return []datastore.Property{
+		{
+			Name:  "ID",
+			Value: s.ID.String(),
+		},
+		{
+			Name:  "ProjectID",
+			Value: s.ProjectID.String(),
+		},
+		{
+			Name:  "Index",
+			Value: s.Index,
+		},
+		{
+			Name:    "Script",
+			Value:   s.Script,
+			NoIndex: true,
+		},
+	}, nil
 }
 
 type ScriptExecution struct {
@@ -28,4 +74,67 @@ type ScriptExecution struct {
 
 func (s *ScriptExecution) NameKey() *datastore.Key {
 	return datastore.NameKey("ScriptExecution", s.ID.String(), nil)
+}
+
+func (s *ScriptExecution) Load(ps []datastore.Property) error {
+	tmp := struct {
+		ID        string
+		ProjectID string
+		Index     int
+		Script    string
+		Value     XDRValue
+		Error     *string
+		Logs      []string
+	}{}
+
+	if err := datastore.LoadStruct(&tmp, ps); err != nil {
+		return err
+	}
+
+	if err := s.ID.UnmarshalText([]byte(tmp.ID)); err != nil {
+		return errors.Wrap(err, "failed to decode UUID")
+	}
+	if err := s.ProjectID.UnmarshalText([]byte(tmp.ProjectID)); err != nil {
+		return errors.Wrap(err, "failed to decode UUID")
+	}
+	s.Index = tmp.Index
+	s.Script = tmp.Script
+	s.Value = tmp.Value
+	s.Error = tmp.Error
+	s.Logs = tmp.Logs
+	return nil
+}
+
+func (s *ScriptExecution) Save() ([]datastore.Property, error) {
+	return []datastore.Property{
+		{
+			Name:  "ID",
+			Value: s.ID.String(),
+		},
+		{
+			Name:  "ProjectID",
+			Value: s.ProjectID.String(),
+		},
+		{
+			Name:  "Index",
+			Value: s.Index,
+		},
+		{
+			Name:    "Script",
+			Value:   s.Script,
+			NoIndex: true,
+		},
+		{
+			Name:  "Value",
+			Value: s.Value,
+		},
+		{
+			Name:  "Error",
+			Value: s.Error,
+		},
+		{
+			Name:  "Logs",
+			Value: s.Logs,
+		},
+	}, nil
 }
