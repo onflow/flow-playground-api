f7160e735db2c91fec361c24337e44b3a00e6561
Bastian Müller <bastian@axiomzen.co>
2020-03-04 14:40:20 -0800

    add error logging middleware, courtesy of CW

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/pkg/errors v0.8.1
 	github.com/prometheus/client_golang v1.3.0
 	github.com/rs/cors v1.6.0
+	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.4.0
 	github.com/vektah/gqlparser v1.2.0
 )
--- server/server.go
+++ server/server.go
@@ -15,6 +15,7 @@ import (
 	"github.com/gorilla/sessions"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/cors"
+	"github.com/sirupsen/logrus"
 
 	playground "github.com/dapperlabs/flow-playground-api"
 	"github.com/dapperlabs/flow-playground-api/middleware"
@@ -94,6 +95,10 @@ func main() {
 		router.Handle("/", handler.Playground("GraphQL playground", "/query"))
 	}
 
+	logger := logrus.StandardLogger()
+	// TODO: set format
+	entry := logrus.NewEntry(logger)
+
 	router.Route("/query", func(r chi.Router) {
 		// Add CORS middleware around every request
 		// See https://github.com/rs/cors for full option listing
@@ -112,7 +117,10 @@ func main() {
 
 		r.Use(middleware.ProjectSessions(cookieStore))
 
-		r.Handle("/", handler.GraphQL(playground.NewExecutableSchema(playground.Config{Resolvers: resolver})))
+		r.Handle("/", handler.GraphQL(
+			playground.NewExecutableSchema(playground.Config{Resolvers: resolver}),
+			handler.RequestMiddleware(middleware.ErrorMiddleware(entry)),
+		))
 	})
 
 	router.Handle("/metrics", promhttp.Handler())
