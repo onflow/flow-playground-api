9efab4610c0a4db212d5c87e02dbe90a4dcf7e77
Peter Siemens <peterjsiemens@gmail.com>
2020-03-04 11:42:39 -0800

    Clean up environment variable config

--- server/server.go
+++ server/server.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 	"strings"
 	"time"
 
@@ -13,6 +12,7 @@ import (
 	"github.com/99designs/gqlgen/handler"
 	"github.com/go-chi/chi"
 	"github.com/gorilla/sessions"
+	"github.com/kelseyhightower/envconfig"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/cors"
 
@@ -24,35 +24,48 @@ import (
 	"github.com/dapperlabs/flow-playground-api/vm"
 )
 
-const (
-	defaultPort                     = "8080"
-	defaultAllowedOrigins           = "http://localhost:3000"
-	defaultSessionAuthenticationKey = "428ce08c21b93e5f0eca24fbeb0c7673"
-	defaultSessionMaxAge            = 157680000 // 5 years in seconds
-	defaultLedgerCacheSize          = 128       // number of ledgers to store in cache
-)
+type Config struct {
+	Port                       int           `default:"8080"`
+	Debug                      bool          `default:"false"`
+	AllowedOrigins             []string      `default:"http://localhost:3000"`
+	SessionAuthKey             string        `default:"428ce08c21b93e5f0eca24fbeb0c7673"`
+	SessionMaxAge              time.Duration `default:"157680000s"`
+	SessionCookiesSecure       bool          `default:"true"`
+	SessionCookiesHTTPOnly     bool          `default:"true"`
+	SessionCookiesSameSiteNone bool          `default:"false"`
+	LedgerCacheSize            int           `default:"128"`
+	StorageBackend             string
+}
+
+type DatastoreConfig struct {
+	GCPProjectID string        `required:"true"`
+	Timeout      time.Duration `default:"5s"`
+}
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-	allowedOrigins := os.Getenv("ALLOWED_ORIGINS")
-	var allowedOriginList []string
-	if allowedOrigins == "" {
-		allowedOriginList = []string{defaultAllowedOrigins}
-	} else {
-		allowedOriginList = strings.Split(os.Getenv("ALLOWED_ORIGINS"), ",")
-	}
-	// If cannot parse, just assume false
-	gqlPlayground, _ := strconv.ParseBool(os.Getenv("GQL_PLAYGROUND"))
+	var conf Config
 
-	storeBackend := os.Getenv("STORE_BACKEND")
+	if err := envconfig.Process("FLOW", &conf); err != nil {
+		log.Fatal(err)
+	}
+
 	var store storage.Store
-	if strings.EqualFold(storeBackend, "datastore") {
+
+	if strings.EqualFold(conf.StorageBackend, "datastore") {
+		var datastoreConf DatastoreConfig
+
+		if err := envconfig.Process("FLOW_DATASTORE", &datastoreConf); err != nil {
+			log.Fatal(err)
+		}
+
 		var err error
-		projectID := os.Getenv("DATASTORE_PROJECT_ID")
-		store, err = datastore.NewDatastore(context.Background(), &datastore.Config{DatastoreProjectID: projectID, DatastoreTimeout: time.Second * 5})
+		store, err = datastore.NewDatastore(
+			context.Background(),
+			&datastore.Config{
+				DatastoreProjectID: datastoreConf.GCPProjectID,
+				DatastoreTimeout:   datastoreConf.Timeout,
+			},
+		)
 		if err != nil {
 			// If datastore is expected, panic when we can't init
 			panic(err)
@@ -61,24 +74,7 @@ func main() {
 		store = memory.NewStore()
 	}
 
-	sessionAuthenticationKey := os.Getenv("SESSION_KEY")
-	if sessionAuthenticationKey == "" {
-		sessionAuthenticationKey = defaultSessionAuthenticationKey
-	}
-
-	var ledgerCacheSize int
-	ledgerCacheSizeStr := os.Getenv("LEDGER_CACHE_SIZE")
-	if ledgerCacheSizeStr == "" {
-		ledgerCacheSize = defaultLedgerCacheSize
-	} else {
-		var err error
-		ledgerCacheSize, err = strconv.Atoi(ledgerCacheSizeStr)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	computer, err := vm.NewComputer(store, ledgerCacheSize)
+	computer, err := vm.NewComputer(store, conf.LedgerCacheSize)
 	if err != nil {
 		panic(err)
 	}
@@ -90,7 +86,7 @@ func main() {
 
 	router := chi.NewRouter()
 
-	if gqlPlayground {
+	if conf.Debug {
 		router.Handle("/", handler.Playground("GraphQL playground", "/query"))
 	}
 
@@ -98,17 +94,20 @@ func main() {
 		// Add CORS middleware around every request
 		// See https://github.com/rs/cors for full option listing
 		r.Use(cors.New(cors.Options{
-			AllowedOrigins:   allowedOriginList,
+			AllowedOrigins:   conf.AllowedOrigins,
 			AllowCredentials: true,
-			Debug:            gqlPlayground,
+			Debug:            conf.Debug,
 		}).Handler)
 
-		cookieStore := sessions.NewCookieStore([]byte(sessionAuthenticationKey))
-		cookieStore.MaxAge(defaultSessionMaxAge)
+		cookieStore := sessions.NewCookieStore([]byte(conf.SessionAuthKey))
+		cookieStore.MaxAge(int(conf.SessionMaxAge.Seconds()))
 
-		// cookieStore.Options.Secure = true
-		cookieStore.Options.HttpOnly = true
-		cookieStore.Options.SameSite = http.SameSiteNoneMode
+		cookieStore.Options.Secure = conf.SessionCookiesSecure
+		cookieStore.Options.HttpOnly = conf.SessionCookiesHTTPOnly
+
+		if conf.SessionCookiesSameSiteNone {
+			cookieStore.Options.SameSite = http.SameSiteNoneMode
+		}
 
 		r.Use(middleware.ProjectSessions(cookieStore))
 
@@ -118,8 +117,8 @@ func main() {
 	router.Handle("/metrics", promhttp.Handler())
 	router.HandleFunc("/ping", ping)
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, router))
+	log.Printf("connect to http://localhost:%d/ for GraphQL playground", conf.Port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", conf.Port), router))
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
