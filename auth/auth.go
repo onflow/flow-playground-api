d672585c53c0fb8d01febfedb2096ca027d7bc11
Peter Siemens <peterjsiemens@gmail.com>
2020-07-13 11:29:21 -0700

    auth: Add documentation to auth package

--- auth/auth.go
+++ auth/auth.go
@@ -12,13 +12,21 @@ import (
 	"github.com/dapperlabs/flow-playground-api/storage"
 )
 
+// An Authenticator manages user authentication for the Playground API.
+//
+// An authenticator instance can create new users, load existing users from session
+// data, and check for project access.
 type Authenticator struct {
 	store       storage.Store
 	sessionName string
 }
 
-const userIDKey = "userID"
-
+// NewAuthenticator returns a new authenticator instance.
+//
+// User data is stored in and loaded from the provided store instance.
+//
+// The session name parameter is used as the cookie name when setting session
+// data on the client.
 func NewAuthenticator(store storage.Store, sessionName string) *Authenticator {
 	return &Authenticator{
 		store:       store,
@@ -26,6 +34,10 @@ func NewAuthenticator(store storage.Store, sessionName string) *Authenticator {
 	}
 }
 
+const userIDKey = "userID"
+
+// GetOrCreateUser gets an existing user from the current session or creates a
+// new user and session if a session does not already exist.
 func (a *Authenticator) GetOrCreateUser(ctx context.Context) (*model.User, error) {
 	session := sessions.Get(ctx, a.sessionName)
 
@@ -54,6 +66,11 @@ func (a *Authenticator) GetOrCreateUser(ctx context.Context) (*model.User, error
 	return user, nil
 }
 
+// CheckProjectAccess returns an error if the current user is not authorized to mutate
+// the provided project.
+//
+// This function checks for access using both the new and legacy authentication schemes. If
+// a user has legacy access, their authentication is then migrated to use the new scheme.
 func (a *Authenticator) CheckProjectAccess(ctx context.Context, proj *model.InternalProject) error {
 	var user *model.User
 	var err error
