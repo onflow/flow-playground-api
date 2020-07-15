32d4ce12c588b91bfa1360ed55035ade10b3e8af
Peter Siemens <peterjsiemens@gmail.com>
2020-07-15 12:12:11 -0700

    model: Fix RegisterDelta for backwards compatibility with old data

--- model/transaction.go
+++ model/transaction.go
@@ -1,13 +1,9 @@
 package model
 
 import (
-	"bytes"
-	"encoding/gob"
 	"encoding/json"
-	"fmt"
 
 	"cloud.google.com/go/datastore"
-	"github.com/dapperlabs/flow-go/engine/execution/state/delta"
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
 )
@@ -178,73 +174,6 @@ func (t *TransactionExecution) Save() ([]datastore.Property, error) {
 		{
 			Name:  "Logs",
 			Value: logs,
-		},
-	}, nil
-}
-
-type RegisterDelta struct {
-	ProjectID uuid.UUID
-	Index     int
-	Delta     delta.Delta
-}
-
-func (r *RegisterDelta) NameKey() *datastore.Key {
-	return datastore.NameKey("RegisterDelta", fmt.Sprintf("%s-%d", r.ProjectID.String(), r.Index), ProjectNameKey(r.ProjectID))
-}
-
-func (r *RegisterDelta) Load(ps []datastore.Property) error {
-	tmp := struct {
-		ProjectID string
-		Index     int
-		Delta     []byte
-	}{}
-
-	if err := datastore.LoadStruct(&tmp, ps); err != nil {
-		return err
-	}
-
-	if err := r.ProjectID.UnmarshalText([]byte(tmp.ProjectID)); err != nil {
-		return errors.Wrap(err, "failed to decode UUID")
-	}
-	r.Index = tmp.Index
-
-	var delta delta.Delta
-
-	decoder := gob.NewDecoder(bytes.NewReader(tmp.Delta))
-	err := decoder.Decode(&delta)
-	if err != nil {
-		return errors.Wrap(err, "failed to decode Delta")
-	}
-
-	r.Delta = delta
-
-	return nil
-}
-
-func (r *RegisterDelta) Save() ([]datastore.Property, error) {
-	w := new(bytes.Buffer)
-
-	encoder := gob.NewEncoder(w)
-	err := encoder.Encode(&r.Delta)
-	if err != nil {
-		return nil, err
-	}
-
-	delta := w.Bytes()
-
-	return []datastore.Property{
-		{
-			Name:  "ProjectID",
-			Value: r.ProjectID.String(),
-		},
-		{
-			Name:  "Index",
-			Value: r.Index,
-		},
-		{
-			Name:    "Delta",
-			Value:   delta,
-			NoIndex: true,
 		},
 	}, nil
 }
