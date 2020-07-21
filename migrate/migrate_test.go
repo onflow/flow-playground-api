079ad4faa38abef0f46a101d3f65006493bf80d5
Peter Siemens <peterjsiemens@gmail.com>
2020-07-21 14:38:03 -0700

    migrate: Add test cases for non migrations

--- migrate/migrate_test.go
+++ migrate/migrate_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/dapperlabs/flow-playground-api/controller"
 	"github.com/dapperlabs/flow-playground-api/migrate"
 	"github.com/dapperlabs/flow-playground-api/model"
+	"github.com/dapperlabs/flow-playground-api/storage"
 	"github.com/dapperlabs/flow-playground-api/storage/memory"
 )
 
@@ -20,33 +21,75 @@ const (
 	cacheSize   = 256
 )
 
-func TestV0ToV0_1_0(t *testing.T) {
-	store := memory.NewStore()
-	computer, err := compute.NewComputer(cacheSize)
-	require.NoError(t, err)
+func TestMigrateNilToV0(t *testing.T) {
+	migrateTest(func(t *testing.T, c migrateTestCase) {
+		projID := uuid.New()
 
-	scripts := controller.NewScripts(store, computer)
-	projects := controller.NewProjects(migrate.V0, store, computer, numAccounts)
+		migrated, err := c.migrator.MigrateProject(projID, nil, migrate.V0)
+		require.NoError(t, err)
+		assert.False(t, migrated)
+	})(t)
+}
 
-	m := migrate.NewMigrator(projects)
+func TestMigrateV0ToV0(t *testing.T) {
+	migrateTest(func(t *testing.T, c migrateTestCase) {
+		projID := uuid.New()
 
-	user := model.User{
-		ID: uuid.New(),
+		migrated, err := c.migrator.MigrateProject(projID, migrate.V0, migrate.V0)
+		require.NoError(t, err)
+		assert.False(t, migrated)
+	})(t)
+}
+
+func TestMigrateV0ToV0_1_0(t *testing.T) {
+	migrateTest(func(t *testing.T, c migrateTestCase) {
+		user := model.User{
+			ID: uuid.New(),
+		}
+
+		err := c.store.InsertUser(&user)
+		require.NoError(t, err)
+
+		proj, err := c.projects.Create(&user, model.NewProject{})
+		require.NoError(t, err)
+
+		assertAllAccountsExist(t, c.scripts, proj)
+
+		migrated, err := c.migrator.MigrateProject(proj.ID, proj.Version, migrate.V0_1_0)
+		require.NoError(t, err)
+		assert.True(t, migrated)
+
+		assertAllAccountsExist(t, c.scripts, proj)
+	})(t)
+}
+
+type migrateTestCase struct {
+	store    storage.Store
+	computer *compute.Computer
+	scripts  *controller.Scripts
+	projects *controller.Projects
+	migrator *migrate.Migrator
+}
+
+func migrateTest(f func(t *testing.T, c migrateTestCase)) func(t *testing.T) {
+	return func(t *testing.T) {
+		store := memory.NewStore()
+		computer, err := compute.NewComputer(cacheSize)
+		require.NoError(t, err)
+
+		scripts := controller.NewScripts(store, computer)
+		projects := controller.NewProjects(migrate.V0, store, computer, numAccounts)
+
+		migrator := migrate.NewMigrator(projects)
+
+		f(t, migrateTestCase{
+			store:    store,
+			computer: computer,
+			scripts:  scripts,
+			projects: projects,
+			migrator: migrator,
+		})
 	}
-
-	err = store.InsertUser(&user)
-	require.NoError(t, err)
-
-	proj, err := projects.Create(&user, model.NewProject{})
-	require.NoError(t, err)
-
-	assertAllAccountsExist(t, scripts, proj)
-
-	migrated, err := m.MigrateProject(proj.ID, proj.Version, migrate.V0_1_0)
-	require.NoError(t, err)
-	assert.True(t, migrated)
-
-	assertAllAccountsExist(t, scripts, proj)
 }
 
 func assertAllAccountsExist(t *testing.T, scripts *controller.Scripts, proj *model.InternalProject) {
