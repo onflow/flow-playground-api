22601cce9090b71ee5572c5396ab21a46374adcd
Max Daunarovich <max.daunarovich@gmail.com>
2020-11-25 18:32:40 +0300

    Add tests for other methods

--- controller/embeds_test.go
+++ controller/embeds_test.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"context"
+	"fmt"
 	"github.com/Masterminds/semver"
 	"github.com/alecthomas/assert"
 	"github.com/dapperlabs/flow-go/engine/execution/state/delta"
@@ -11,9 +13,12 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
+// TODO: can we move this to another file?
+// Mock store requres implementation of all those methods
 type MockStore struct {
 }
 
@@ -141,6 +146,7 @@ func (ms *MockStore) GetScriptTemplate(id model.ProjectChildID, tmpl *model.Scri
 	return nil
 }
 
+// Utility method to send requests
 func testRequest(t *testing.T, ts *httptest.Server, method, path string, body io.Reader) (*http.Response, string) {
 	req, err := http.NewRequest(method, ts.URL+path, body)
 	if err != nil {
@@ -164,6 +170,7 @@ func testRequest(t *testing.T, ts *httptest.Server, method, path string, body io
 	return resp, string(respBody)
 }
 
+// Unit tests start here
 func TestEmbedsHandler_ServeHTTP(t *testing.T) {
 	mockStore := MockStore{}
 	playgroundBase := "http://localhost:3000"
@@ -180,4 +187,101 @@ func TestEmbedsHandler_ServeHTTP(t *testing.T) {
 	response, _ := testRequest(t, ts, "GET", snippetUrl, nil)
 
 	assert.Equal(t, response.StatusCode, http.StatusOK)
+}
+
+func TestEmbedsHandler_getUUID(t *testing.T) {
+	projectId := "24278e82-9316-4559-96f2-573ec58f618f"
+	scriptType := "script"
+	scriptId := "9473b82c-36ea-4810-ad3f-7ea5497d9cae"
+
+	r := httptest.NewRequest("GET", "http://play.onflow.org", nil)
+
+	rctx := chi.NewRouteContext()
+	rctx.URLParams.Add("projectID", projectId)
+	rctx.URLParams.Add("scriptType", scriptType)
+	rctx.URLParams.Add("scriptId", scriptId)
+
+	r = r.WithContext(context.WithValue(r.Context(), chi.RouteCtxKey, rctx))
+
+	// Check that projectUUID extracted properly
+	projectUUID, err := getUUID("projectID", r)
+	if err != nil {
+		t.Fail()
+	}
+	expected, _ := uuid.Parse(projectId)
+	assert.Equal(t, projectUUID, expected, projectUUID)
+
+	// Check that scriptId extracted properly
+	scriptUUID, err := getUUID("scriptId", r)
+	if err != nil {
+		t.Fail()
+	}
+	expected, _ = uuid.Parse(scriptId)
+	assert.Equal(t, scriptUUID, expected, projectUUID)
+
+	scriptTypeParam, _ := getURLParam("scriptType", r)
+	assert.Equal(t, scriptTypeParam, scriptType)
+}
+
+func TestEmbedsHandler_createCodeStyles(t *testing.T) {
+	const styles = ".chroma { background: red }"
+	const styleName = "red"
+	codeStyle := createCodeStyles(styles, styleName)
+
+	// Check that id is created properly
+	themeName := fmt.Sprintf("theme-%s", styleName)
+	haveThemeString := strings.Contains(codeStyle, themeName)
+	assert.True(t, haveThemeString)
+
+	// Chroma class names shall have another class to ensure themes are working properly
+	adjustedClassName := fmt.Sprintf(".chroma.%s", styleName)
+	adjustedStyles := strings.ReplaceAll(styles, ".chroma", adjustedClassName)
+
+	// Check that inserted HTML have adjusted styles
+	haveStylesHTML := strings.Contains(codeStyle, adjustedStyles)
+	assert.True(t, haveStylesHTML)
+}
+
+func TestEmbedsHandler_generateWrapperStyles(t *testing.T) {
+	generatedStyles := generateWrapperStyles()
+
+	testClasses := [...]string{
+		"cadence-snippet",
+		"cadence-snippet pre.chroma",
+		".cadence-code-block .chroma",
+		".cadence-info-block",
+		".cadence-info-block img",
+		".cadence-info-block a",
+		".flow-playground-logo",
+		".cadence-info-block .umbrella",
+	}
+
+	for _, value := range testClasses {
+		assert.True(t, strings.Contains(generatedStyles, value))
+	}
+}
+
+func TestEmbedsHandler_createSnippetStyles(t *testing.T) {
+	snippetStyles := createSnippetStyles()
+	generatedStyles := generateWrapperStyles()
+
+	haveProperId := strings.Contains(snippetStyles, "cadence-styles")
+	assert.True(t, haveProperId)
+
+	haveStylesHTML := strings.Contains(snippetStyles, generatedStyles)
+	assert.True(t, haveStylesHTML)
+}
+
+func TestEmbedsHandler_wrapCodeBlock(t *testing.T) {
+	htmlBlock := "<div>no code</div>"
+	styleName := "red"
+	playgroundUrl := "https://play.onflow.org/"
+
+	wrappedCodeBlock := wrapCodeBlock(htmlBlock, styleName, playgroundUrl)
+
+	haveWrappedCode := strings.Contains(wrappedCodeBlock, htmlBlock)
+	assert.True(t, haveWrappedCode)
+
+	haveProperUrl := strings.Contains(wrappedCodeBlock, playgroundUrl)
+	assert.True(t, haveProperUrl)
 }
