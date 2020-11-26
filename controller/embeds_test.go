3b3a24c2405dfe4772c3113c073ed0da39c66497
Max Daunarovich <max.daunarovich@gmail.com>
2020-11-26 19:18:04 +0300

    Refactor tests

--- controller/embeds_test.go
+++ controller/embeds_test.go
@@ -3,12 +3,12 @@ package controller
 import (
 	"context"
 	"fmt"
-	"github.com/Masterminds/semver"
 	"github.com/alecthomas/assert"
-	"github.com/dapperlabs/flow-go/engine/execution/state/delta"
 	"github.com/dapperlabs/flow-playground-api/model"
+	"github.com/dapperlabs/flow-playground-api/storage/memory"
 	"github.com/go-chi/chi"
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -16,135 +16,6 @@ import (
 	"strings"
 	"testing"
 )
-
-// TODO: can we move this to another file?
-// Mock store requres implementation of all those methods
-type MockStore struct {
-}
-
-func (ms *MockStore) InsertUser(user *model.User) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) GetUser(id uuid.UUID, user *model.User) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) CreateProject(proj *model.InternalProject, registerDeltas []delta.Delta, accounts []*model.InternalAccount, ttpl []*model.TransactionTemplate, stpl []*model.ScriptTemplate) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) UpdateProject(input model.UpdateProject, proj *model.InternalProject) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) UpdateProjectOwner(id, userID uuid.UUID) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) UpdateProjectVersion(id uuid.UUID, version *semver.Version) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) ResetProjectState(newDeltas []delta.Delta, proj *model.InternalProject) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) GetProject(id uuid.UUID, proj *model.InternalProject) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) InsertAccount(acc *model.InternalAccount) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) GetAccount(id model.ProjectChildID, acc *model.InternalAccount) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) UpdateAccount(input model.UpdateAccount, acc *model.InternalAccount) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) UpdateAccountAfterDeployment(input model.UpdateAccount, states map[uuid.UUID]model.AccountState, delta delta.Delta, acc *model.InternalAccount) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) GetAccountsForProject(projectID uuid.UUID, accs *[]*model.InternalAccount) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) DeleteAccount(id model.ProjectChildID) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) InsertTransactionTemplate(tpl *model.TransactionTemplate) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) UpdateTransactionTemplate(input model.UpdateTransactionTemplate, tpl *model.TransactionTemplate) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) GetTransactionTemplate(id model.ProjectChildID, tpl *model.TransactionTemplate) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) GetTransactionTemplatesForProject(projectID uuid.UUID, tpls *[]*model.TransactionTemplate) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) DeleteTransactionTemplate(id model.ProjectChildID) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) InsertTransactionExecution(exe *model.TransactionExecution, states map[uuid.UUID]model.AccountState, delta delta.Delta) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) GetTransactionExecutionsForProject(projectID uuid.UUID, exes *[]*model.TransactionExecution) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) InsertScriptTemplate(tpl *model.ScriptTemplate) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) UpdateScriptTemplate(input model.UpdateScriptTemplate, tpl *model.ScriptTemplate) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) GetScriptTemplatesForProject(projectID uuid.UUID, tpls *[]*model.ScriptTemplate) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) DeleteScriptTemplate(id model.ProjectChildID) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) InsertScriptExecution(exe *model.ScriptExecution) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) GetScriptExecutionsForProject(projectID uuid.UUID, exes *[]*model.ScriptExecution) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) GetRegisterDeltasForProject(projectID uuid.UUID, deltas *[]*model.RegisterDelta) error {
-	panic("implement me")
-}
-
-func (ms *MockStore) GetScriptTemplate(id model.ProjectChildID, tmpl *model.ScriptTemplate) error {
-	tmpl.Title = "test contract"
-	tmpl.Index = 1
-	tmpl.Script = `
-		pub fun main(): Int {
-			return 42
-		}
-	`
-
-	return nil
-}
 
 // Utility method to send requests
 func testRequest(t *testing.T, ts *httptest.Server, method, path string, body io.Reader) (*http.Response, string) {
@@ -172,21 +43,44 @@ func testRequest(t *testing.T, ts *httptest.Server, method, path string, body io
 
 // Unit tests start here
 func TestEmbedsHandler_ServeHTTP(t *testing.T) {
-	mockStore := MockStore{}
-	playgroundBase := "http://localhost:3000"
-	embedsHandler := NewEmbedsHandler(&mockStore, playgroundBase)
+	store := memory.NewStore()
+	playgroundBaseURL := "http://localhost:3000"
+	embedsHandler := NewEmbedsHandler(store, playgroundBaseURL)
+
+	projectID := "24278e82-9316-4559-96f2-573ec58f618f"
+	scriptType := "script"
+	scriptID := "9473b82c-36ea-4810-ad3f-7ea5497d9cae"
+
+	script := `
+	pub fun main(): Int {
+	  return 42
+	}
+	`
+
+	scriptTemplate := model.ScriptTemplate{
+		ProjectChildID: model.ProjectChildID{
+			ID:        uuid.MustParse(scriptID),
+			ProjectID: uuid.MustParse(projectID),
+		},
+		Title:  "test contract",
+		Script: script,
+	}
+
+	// insert your mock data
+	err := store.InsertScriptTemplate(&scriptTemplate)
+	require.NoError(t, err)
 
 	r := chi.NewRouter()
-	r.Get("/embed/{projectID}/{scriptType}/{scriptId}", embedsHandler.ServeHTTP)
+	r.Get("/embed", embedsHandler.ServeHTTP)
 
 	ts := httptest.NewServer(r)
 	defer ts.Close()
 
-	snippetUrl := "/embed/24278e82-9316-4559-96f2-573ec58f618f/script/9473b82c-36ea-4810-ad3f-7ea5497d9cae"
+	snippetUrl := fmt.Sprintf("/embed/%s/%s/%s", projectID, scriptType, scriptID)
 
 	response, _ := testRequest(t, ts, "GET", snippetUrl, nil)
 
-	assert.Equal(t, response.StatusCode, http.StatusOK)
+	assert.Equal(t, http.StatusOK, response.StatusCode)
 }
 
 func TestEmbedsHandler_getUUID(t *testing.T) {
@@ -194,12 +88,10 @@ func TestEmbedsHandler_getUUID(t *testing.T) {
 	scriptType := "script"
 	scriptId := "9473b82c-36ea-4810-ad3f-7ea5497d9cae"
 
-	r := httptest.NewRequest("GET", "http://play.onflow.org", nil)
+	requestURL := fmt.Sprintf("http://playground-api.com/embed?project=%s&type=%s&id=%s", projectId, scriptType, scriptId)
+	r := httptest.NewRequest("GET", requestURL, nil)
 
 	rctx := chi.NewRouteContext()
-	rctx.URLParams.Add("projectID", projectId)
-	rctx.URLParams.Add("scriptType", scriptType)
-	rctx.URLParams.Add("scriptId", scriptId)
 
 	r = r.WithContext(context.WithValue(r.Context(), chi.RouteCtxKey, rctx))
 
@@ -209,7 +101,7 @@ func TestEmbedsHandler_getUUID(t *testing.T) {
 		t.Fail()
 	}
 	expected, _ := uuid.Parse(projectId)
-	assert.Equal(t, projectUUID, expected, projectUUID)
+	assert.Equal(t, expected, projectUUID)
 
 	// Check that scriptId extracted properly
 	scriptUUID, err := getUUID("scriptId", r)
@@ -217,10 +109,10 @@ func TestEmbedsHandler_getUUID(t *testing.T) {
 		t.Fail()
 	}
 	expected, _ = uuid.Parse(scriptId)
-	assert.Equal(t, scriptUUID, expected, projectUUID)
+	assert.Equal(t, expected, scriptUUID)
 
 	scriptTypeParam, _ := getURLParam("scriptType", r)
-	assert.Equal(t, scriptTypeParam, scriptType)
+	assert.Equal(t, scriptType, scriptTypeParam)
 }
 
 func TestEmbedsHandler_createCodeStyles(t *testing.T) {
