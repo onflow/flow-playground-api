ca5f191456b0ccb3b7c836f33da2b28882f08eb3
Bastian Müller <bastian@axiomzen.co>
2021-01-19 16:00:00 -0800

    add license headers

--- controller/embeds_test.go
+++ controller/embeds_test.go
@@ -1,3 +1,21 @@
+/*
+ * Flow Playground
+ *
+ * Copyright 2019-2021 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controller
 
 import (
@@ -5,11 +23,11 @@ import (
 	"fmt"
 	"github.com/Masterminds/semver"
 	"github.com/alecthomas/assert"
-	"github.com/onflow/flow-go/engine/execution/state/delta"
 	"github.com/dapperlabs/flow-playground-api/model"
 	"github.com/dapperlabs/flow-playground-api/storage/memory"
 	"github.com/go-chi/chi"
 	"github.com/google/uuid"
+	"github.com/onflow/flow-go/engine/execution/state/delta"
 	"github.com/stretchr/testify/require"
 	"io"
 	"io/ioutil"
