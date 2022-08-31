08dce8e11a983a75b88ec7ff8df5f923b5f17f93
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-08-31 16:11:10 +0200

    refactor resolver with new controllers

--- resolver.go
+++ resolver.go
@@ -20,26 +20,16 @@ package playground
 
 import (
 	"context"
-	"fmt"
-
-	jsoncdc "github.com/onflow/cadence/encoding/json"
 
 	"github.com/Masterminds/semver"
 	"github.com/dapperlabs/flow-playground-api/auth"
 	"github.com/dapperlabs/flow-playground-api/blockchain"
-	"github.com/dapperlabs/flow-playground-api/compute"
 	"github.com/dapperlabs/flow-playground-api/controller"
 	"github.com/dapperlabs/flow-playground-api/migrate"
 	"github.com/dapperlabs/flow-playground-api/model"
 	"github.com/dapperlabs/flow-playground-api/storage"
 	"github.com/google/uuid"
 	"github.com/onflow/cadence"
-	"github.com/onflow/cadence/runtime/ast"
-	"github.com/onflow/cadence/runtime/common"
-	"github.com/onflow/cadence/runtime/parser"
-	"github.com/onflow/flow-go-sdk"
-	"github.com/onflow/flow-go/engine/execution/state/delta"
-	flowgo "github.com/onflow/flow-go/model/flow"
 	"github.com/pkg/errors"
 )
 
@@ -52,6 +42,8 @@ type Resolver struct {
 	migrator           *migrate.Migrator
 	projects           *controller.Projects
 	scripts            *controller.Scripts
+	transactions       *controller.Transactions
+	accounts           *controller.Accounts
 	lastCreatedProject *model.InternalProject
 	blockchain         blockchain.Blockchain
 }
@@ -64,16 +56,20 @@ func NewResolver(
 ) *Resolver {
 	projects := controller.NewProjects(version, store, MaxAccounts, blockchain)
 	scripts := controller.NewScripts(store, blockchain)
+	transactions := controller.NewTransactions(store, blockchain)
+	accounts := controller.NewAccounts(store, blockchain)
 	migrator := migrate.NewMigrator(projects)
 
 	return &Resolver{
-		version:    version,
-		store:      store,
-		auth:       auth,
-		migrator:   migrator,
-		projects:   projects,
-		scripts:    scripts,
-		blockchain: blockchain,
+		version:      version,
+		store:        store,
+		auth:         auth,
+		migrator:     migrator,
+		projects:     projects,
+		scripts:      scripts,
+		transactions: transactions,
+		accounts:     accounts,
+		blockchain:   blockchain,
 	}
 }
 
@@ -137,155 +133,11 @@ func (r *mutationResolver) UpdateProject(ctx context.Context, input model.Update
 	return proj.ExportPublicMutable(), nil
 }
 
-func (r *mutationResolver) UpdateAccount(ctx context.Context, input model.UpdateAccount) (*model.Account, error) {
-
-	var proj model.InternalProject
-
-	err := r.projects.Get(input.ProjectID, &proj)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get project")
-	}
-
-	if err := r.auth.CheckProjectAccess(ctx, &proj); err != nil {
-		return nil, err
-	}
-
-	var acc model.InternalAccount
-
-	err = r.store.GetAccount(model.NewProjectChildID(input.ID, proj.ID), &acc)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get account")
-	}
-
-	if input.DeployedCode == nil {
-		err = r.store.UpdateAccount(input, &acc)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to update account")
-		}
-
-		return acc.Export(), nil
-	}
-
-	// Redeploy: clear all state
-	if acc.DeployedCode != "" {
-		err := r.projects.Reset(&proj)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to clear project state")
-		}
-	}
-
-	result, contractName, err := r.blockchain.DeployContract(acc.Address, *input.DeployedCode)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to deploy account code")
-	}
-
-	if result.Error != nil {
-		return nil, errors.Wrap(result.Error, "failed to deploy account code")
-	}
-
-	input.DeployedContracts = &[]string{contractName}
-
-	err = r.store.UpdateAccountAfterDeployment(input, nil, delta.NewDelta(), &acc) // todo refactor
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to update account")
-	}
-
-	return acc.Export(), nil
-}
-
-func getSourceContractName(code string) (string, error) {
-	program, err := parser.ParseProgram(code, nil)
-	if err != nil {
-		return "", err
-	}
-	return getProgramContractName(program)
-}
-
-func getProgramContractName(program *ast.Program) (string, error) {
-
-	// The code may declare exactly one contract or one contract interface.
-
-	var contractDeclarations []*ast.CompositeDeclaration
-	var contractInterfaceDeclarations []*ast.InterfaceDeclaration
-
-	for _, compositeDeclaration := range program.CompositeDeclarations() {
-		if compositeDeclaration.CompositeKind == common.CompositeKindContract {
-			contractDeclarations = append(contractDeclarations, compositeDeclaration)
-		} else {
-			return "", fmt.Errorf(
-				"invalid %s: the code must declare exactly one contract or contract interface",
-				compositeDeclaration.DeclarationKind().Name(),
-			)
-		}
-	}
-
-	for _, interfaceDeclaration := range program.InterfaceDeclarations() {
-		if interfaceDeclaration.CompositeKind == common.CompositeKindContract {
-			contractInterfaceDeclarations = append(contractInterfaceDeclarations, interfaceDeclaration)
-		} else {
-			return "", fmt.Errorf(
-				"invalid %s: the code must declare exactly one contract or contract interface",
-				interfaceDeclaration.DeclarationKind().Name(),
-			)
-		}
-	}
-
-	switch {
-	case len(contractDeclarations) == 1 && len(contractInterfaceDeclarations) == 0:
-		return contractDeclarations[0].Identifier.Identifier, nil
-	case len(contractInterfaceDeclarations) == 1 && len(contractDeclarations) == 0:
-		return contractInterfaceDeclarations[0].Identifier.Identifier, nil
-	default:
-		return "", errors.New(
-			"the code must declare exactly one contract or contract interface",
-		)
-	}
-}
-
-func (r *mutationResolver) getAccountStates(
-	projectID uuid.UUID,
-	newStates compute.AccountStates,
-) (map[uuid.UUID]model.AccountState, error) {
-	var accounts []*model.InternalAccount
-
-	err := r.store.GetAccountsForProject(projectID, &accounts)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get project accounts")
-	}
-
-	states := make(map[uuid.UUID]model.AccountState)
-
-	for _, account := range accounts {
-		stateDelta, ok := newStates[account.Address]
-		if !ok {
-			continue
-		}
-
-		state, err := account.State()
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to get account state")
-		}
-
-		for key, value := range stateDelta {
-			state[key] = value
-		}
-
-		states[account.ID] = state
-	}
-
-	return states, nil
+func (r *mutationResolver) UpdateAccount(ctx context.Context, input any) (*model.Account, error) {
+	panic("not implemented")
 }
 
 func (r *mutationResolver) CreateTransactionTemplate(ctx context.Context, input model.NewTransactionTemplate) (*model.TransactionTemplate, error) {
-	tpl := &model.TransactionTemplate{
-		ProjectChildID: model.ProjectChildID{
-			ID:        uuid.New(),
-			ProjectID: input.ProjectID,
-		},
-		Title:  input.Title,
-		Script: input.Script,
-	}
-
 	var proj model.InternalProject
 
 	err := r.projects.Get(input.ProjectID, &proj)
@@ -297,17 +149,10 @@ func (r *mutationResolver) CreateTransactionTemplate(ctx context.Context, input 
 		return nil, err
 	}
 
-	err = r.store.InsertTransactionTemplate(tpl)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to store transaction template")
-	}
-
-	return tpl, nil
+	return r.transactions.CreateTemplate(input.ProjectID, input.Title, input.Script)
 }
 
 func (r *mutationResolver) UpdateTransactionTemplate(ctx context.Context, input model.UpdateTransactionTemplate) (*model.TransactionTemplate, error) {
-	var tpl model.TransactionTemplate
-
 	var proj model.InternalProject
 
 	err := r.projects.Get(input.ProjectID, &proj)
@@ -319,12 +164,7 @@ func (r *mutationResolver) UpdateTransactionTemplate(ctx context.Context, input 
 		return nil, err
 	}
 
-	err = r.store.UpdateTransactionTemplate(input, &tpl)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to update transaction template")
-	}
-
-	return &tpl, nil
+	return r.transactions.UpdateTemplate(input)
 }
 
 func (r *mutationResolver) DeleteTransactionTemplate(ctx context.Context, id uuid.UUID, projectID uuid.UUID) (uuid.UUID, error) {
@@ -339,9 +179,9 @@ func (r *mutationResolver) DeleteTransactionTemplate(ctx context.Context, id uui
 		return uuid.Nil, err
 	}
 
-	err = r.store.DeleteTransactionTemplate(model.NewProjectChildID(id, projectID))
+	err = r.transactions.DeleteTemplate(id, projectID)
 	if err != nil {
-		return uuid.Nil, errors.Wrap(err, "failed to delete transaction template")
+		return id, err
 	}
 
 	return id, nil
@@ -362,41 +202,7 @@ func (r *mutationResolver) CreateTransactionExecution(
 		return nil, err
 	}
 
-	result, err := r.blockchain.ExecuteTransaction(input.Script, input.Arguments, input.Signers)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to execute transaction")
-	}
-
-	// todo refactor so the model has factory method that creates this model from transaction and serialises events, errors etc
-	exe := model.TransactionExecution{
-		ProjectChildID: model.ProjectChildID{
-			ID:        uuid.New(),
-			ProjectID: input.ProjectID,
-		},
-		Script:    input.Script,
-		Arguments: input.Arguments,
-		Logs:      result.Logs,
-	}
-
-	var states map[uuid.UUID]model.AccountState
-
-	if result.Error != nil {
-		exe.Errors = compute.ExtractProgramErrors(result.Error)
-	}
-
-	events, err := parseEvents(result.Events)
-	if err != nil {
-		return nil, err
-	}
-
-	exe.Events = events
-
-	err = r.store.InsertTransactionExecution(&exe, states, delta.NewDelta()) // todo refactor delta
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to insert transaction execution record")
-	}
-
-	return &exe, nil
+	return r.transactions.CreateTransactionExecution(input)
 }
 
 func (r *mutationResolver) CreateScriptTemplate(ctx context.Context, input model.NewScriptTemplate) (*model.ScriptTemplate, error) {
@@ -431,14 +237,7 @@ func (r *mutationResolver) UpdateScriptTemplate(ctx context.Context, input model
 		return nil, err
 	}
 
-	var tpl model.ScriptTemplate
-
-	err = r.scripts.UpdateTemplate(input, &tpl)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to update script template")
-	}
-
-	return &tpl, nil
+	return r.scripts.UpdateTemplate(input)
 }
 
 func (r *mutationResolver) DeleteScriptTemplate(
@@ -490,68 +289,30 @@ func (r *mutationResolver) CreateScriptExecution(
 
 type projectResolver struct{ *Resolver }
 
-func (r *projectResolver) Accounts(ctx context.Context, obj *model.Project) ([]*model.Account, error) {
-	var accs []*model.InternalAccount
-
-	err := r.store.GetAccountsForProject(obj.ID, &accs)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get accounts")
-	}
-
-	exportedAccs := make([]*model.Account, len(accs))
-
-	for i, acc := range accs {
-		exported, err := acc.ExportWithJSONState()
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to export")
-		}
-
-		exportedAccs[i] = exported
-	}
-
-	return exportedAccs, nil
+func (r *projectResolver) Accounts(_ context.Context, proj *model.Project) ([]*model.Account, error) {
+	return r.accounts.AllForProjectID(proj.ID)
 }
 
-func (r *projectResolver) TransactionTemplates(ctx context.Context, obj *model.Project) ([]*model.TransactionTemplate, error) {
-	var tpls []*model.TransactionTemplate
-
-	err := r.store.GetTransactionTemplatesForProject(obj.ID, &tpls)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get transaction templates")
-	}
-
-	return tpls, nil
+func (r *projectResolver) TransactionTemplates(_ context.Context, proj *model.Project) ([]*model.TransactionTemplate, error) {
+	return r.transactions.AllTemplatesForProjectID(proj.ID)
 }
 
-func (r *projectResolver) TransactionExecutions(ctx context.Context, obj *model.Project) ([]*model.TransactionExecution, error) {
-	var exes []*model.TransactionExecution
-
-	err := r.store.GetTransactionExecutionsForProject(obj.ID, &exes)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get transaction executions")
-	}
-
-	return exes, nil
+func (r *projectResolver) TransactionExecutions(_ context.Context, proj *model.Project) ([]*model.TransactionExecution, error) {
+	return r.transactions.AllExecutionsForProjectID(proj.ID)
 }
 
-func (r *projectResolver) ScriptTemplates(ctx context.Context, obj *model.Project) ([]*model.ScriptTemplate, error) {
-	var tpls []*model.ScriptTemplate
-
-	err := r.store.GetScriptTemplatesForProject(obj.ID, &tpls)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get script templates")
-	}
-
-	return tpls, nil
+func (r *projectResolver) ScriptTemplates(_ context.Context, proj *model.Project) ([]*model.ScriptTemplate, error) {
+	return r.scripts.AllTemplatesForProjectID(proj.ID)
 }
 
-func (r *projectResolver) ScriptExecutions(ctx context.Context, obj *model.Project) ([]*model.ScriptExecution, error) {
+func (r *projectResolver) ScriptExecutions(_ context.Context, _ *model.Project) ([]*model.ScriptExecution, error) {
+	// todo implement
 	panic("not implemented")
 }
 
 type queryResolver struct{ *Resolver }
 
-func (r *queryResolver) PlaygroundInfo(ctx context.Context) (*model.PlaygroundInfo, error) {
+func (r *queryResolver) PlaygroundInfo(_ context.Context) (*model.PlaygroundInfo, error) {
 	return &model.PlaygroundInfo{
 		APIVersion:     *r.version,
 		CadenceVersion: *semver.MustParse(cadence.Version),
@@ -589,88 +350,20 @@ func (r *queryResolver) Project(ctx context.Context, id uuid.UUID) (*model.Proje
 	return proj.ExportPublicMutable(), nil
 }
 
-func (r *queryResolver) Account(ctx context.Context, id uuid.UUID, projectID uuid.UUID) (*model.Account, error) {
-	var acc model.InternalAccount
-
-	err := r.store.GetAccount(model.NewProjectChildID(id, projectID), &acc)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get account")
-	}
-
-	exported, err := acc.ExportWithJSONState()
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to export")
-	}
-
-	return exported, nil
+func (r *queryResolver) Account(_ context.Context, id uuid.UUID, projectID uuid.UUID) (*model.Account, error) {
+	return r.accounts.GetByID(id, projectID)
 }
 
-func (r *queryResolver) TransactionTemplate(ctx context.Context, id uuid.UUID, projectID uuid.UUID) (*model.TransactionTemplate, error) {
-	var tpl model.TransactionTemplate
-
-	err := r.store.GetTransactionTemplate(model.NewProjectChildID(id, projectID), &tpl)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get transaction template")
-	}
-
-	return &tpl, nil
+func (r *queryResolver) TransactionTemplate(_ context.Context, id uuid.UUID, projectID uuid.UUID) (*model.TransactionTemplate, error) {
+	return r.transactions.TemplateByID(id, projectID)
 }
 
-func (r *queryResolver) ScriptTemplate(ctx context.Context, id uuid.UUID, projectID uuid.UUID) (*model.ScriptTemplate, error) {
-	var tpl model.ScriptTemplate
-
-	err := r.store.GetScriptTemplate(model.NewProjectChildID(id, projectID), &tpl)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get script template")
-	}
-
-	return &tpl, nil
+func (r *queryResolver) ScriptTemplate(_ context.Context, id uuid.UUID, projectID uuid.UUID) (*model.ScriptTemplate, error) {
+	return r.scripts.TemplateByID(id, projectID)
 }
 
 type transactionExecutionResolver struct{ *Resolver }
 
 func (*transactionExecutionResolver) Signers(_ context.Context, _ *model.TransactionExecution) ([]*model.Account, error) {
 	panic("not implemented")
-}
-
-func parseEvents(flowEvents []flow.Event) ([]model.Event, error) {
-	events := make([]model.Event, len(flowEvents))
-
-	for i, event := range flowEvents {
-		parsedEvent, err := parseEvent(event)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to parse event")
-		}
-		events[i] = parsedEvent
-	}
-
-	return events, nil
-}
-
-func parseEvent(event flow.Event) (model.Event, error) {
-	values := make([]string, len(event.Value.Fields))
-	for j, field := range event.Value.Fields {
-		encoded, _ := jsoncdc.Encode(field)
-		values[j] = string(encoded)
-	}
-
-	return model.Event{
-		Type:   event.Type,
-		Values: values,
-	}, nil
-}
-
-func toTransactionBody(tx *flow.Transaction) *flowgo.TransactionBody {
-	txBody := flowgo.NewTransactionBody()
-	txBody.SetScript(tx.Script)
-
-	for _, authorizer := range tx.Authorizers {
-		txBody.AddAuthorizer(flowgo.Address(authorizer))
-	}
-
-	for _, arg := range tx.Arguments {
-		txBody.AddArgument(arg)
-	}
-
-	return txBody
 }
