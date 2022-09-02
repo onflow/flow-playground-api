d97c36e9d9aebca4b9bdb8807da6def7ac7a12b8
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-09-02 13:13:33 +0200

    fix embed

--- controller/embeds_test.go
+++ controller/embeds_test.go
@@ -32,7 +32,6 @@ import (
 	"github.com/alecthomas/assert"
 	"github.com/go-chi/chi"
 	"github.com/google/uuid"
-	"github.com/onflow/flow-go/engine/execution/state/delta"
 	"github.com/stretchr/testify/require"
 
 	"github.com/dapperlabs/flow-playground-api/model"
@@ -116,14 +115,12 @@ func TestEmbedsHandler_ServeHTTP(t *testing.T) {
 		Version:  version,
 	}
 
-	accounts := make([]*model.InternalAccount, 0)
-	deltas := make([]delta.Delta, 0)
 	ttpls := make([]*model.TransactionTemplate, 0)
 	stpls := make([]*model.ScriptTemplate, 0)
 
 	internalProj.UserID = user.ID
 
-	projErr := store.CreateProject(internalProj, deltas, accounts, ttpls, stpls)
+	projErr := store.CreateProject(internalProj, ttpls, stpls)
 	require.NoError(t, projErr)
 
 	script := `
