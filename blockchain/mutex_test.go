eb3660e3fe11418fcb077d414377c1f5003e90e7
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-09-20 11:00:53 +0200

    add mutex test

---
+++ blockchain/mutex_test.go
@@ -0,0 +1,46 @@
+package blockchain
+
+import (
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_Mutex(t *testing.T) {
+	mutex := newMutex()
+
+	testUuid := uuid.New()
+
+	m := mutex.load(testUuid)
+	m.Lock()
+
+	v, _ := mutex.muCounter.Load(testUuid)
+	assert.Equal(t, 1, v.(int))
+
+	_, exists := mutex.mu.Load(testUuid)
+	assert.True(t, exists)
+
+	m1 := mutex.load(testUuid)
+	locked := m1.TryLock() // should fail since we already have one lock
+	assert.False(t, locked)
+
+	v, _ = mutex.muCounter.Load(testUuid)
+	assert.Equal(t, 2, v.(int))
+
+	mutex.remove(testUuid).Unlock()
+
+	v, _ = mutex.muCounter.Load(testUuid)
+	assert.Equal(t, 1, v.(int))
+
+	locked = m1.TryLock()
+	assert.True(t, locked) // should succeed now
+
+	mutex.remove(testUuid).Unlock()
+
+	// after all locks are released there shouldn't be any counter left
+	_, counterExists := mutex.muCounter.Load(testUuid)
+	assert.False(t, counterExists)
+
+	_, exists = mutex.mu.Load(testUuid)
+	assert.False(t, exists)
+}
