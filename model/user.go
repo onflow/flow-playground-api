7af229e5eb79dd931d8bc6b55b1179633c3ed26e
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-09-21 16:12:08 +0200

    remove persistence specific models

--- model/user.go
+++ model/user.go
@@ -19,40 +19,9 @@
 package model
 
 import (
-	"cloud.google.com/go/datastore"
 	"github.com/google/uuid"
-	"github.com/pkg/errors"
 )
 
 type User struct {
 	ID uuid.UUID
-}
-
-func (u *User) NameKey() *datastore.Key {
-	return datastore.NameKey("User", u.ID.String(), nil)
-}
-
-func (u *User) Load(ps []datastore.Property) error {
-	tmp := struct {
-		ID string
-	}{}
-
-	if err := datastore.LoadStruct(&tmp, ps); err != nil {
-		return err
-	}
-
-	if err := u.ID.UnmarshalText([]byte(tmp.ID)); err != nil {
-		return errors.Wrap(err, "failed to decode UUID")
-	}
-
-	return nil
-}
-
-func (u *User) Save() ([]datastore.Property, error) {
-	return []datastore.Property{
-		{
-			Name:  "ID",
-			Value: u.ID.String(),
-		},
-	}, nil
 }
