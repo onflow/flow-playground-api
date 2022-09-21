80d354a9b6821ea991cb423b5d229aceb061e9da
Dylan Tinianov <dylantinianov@gmail.com>
2022-09-21 13:39:06 -0700

    Add migrateTransactionTemplate

--- migrate/sqlMigrator/sqlMigrator.go
+++ migrate/sqlMigrator/sqlMigrator.go
@@ -4,7 +4,11 @@ package sqlMigrator
 import (
 	"context"
 	"fmt"
+	"github.com/dapperlabs/flow-playground-api/migrate/sqlMigrator/model"
 	"github.com/dapperlabs/flow-playground-api/migrate/sqlMigrator/storage/datastore"
+	sqlModel "github.com/dapperlabs/flow-playground-api/model"
+	"github.com/dapperlabs/flow-playground-api/storage/sql"
+	"github.com/google/uuid"
 	"time"
 )
 
@@ -19,7 +23,90 @@ func main() {
 		return
 	}
 
+	// TODO: Connect to SQL db
+	sqldb := sql.NewPostgreSQL()
+
 	fmt.Println("Obtaining all projects from datastore...")
 
-	// Get all projects for migration
+	// TODO: Get all projects for migration
+	id, _ := uuid.NewUUID()
+
+	var test = []int{1, 2, 3}
+
+	// Get each project from datastore and migrate to sql
+	for i, _ := range test {
+		proj := &model.InternalProject{}
+		err := store.GetProject(id, proj)
+		if err != nil {
+			// TODO: handle error
+			fmt.Println("Could not get project", err)
+		}
+
+		sqlProj := migrateProject(proj)
+
+		// Drop projects that aren't supposed to be persisted
+		if !sqlProj.Persist {
+			continue
+		}
+
+		// Get script and transaction templates
+		var ttpl []*model.TransactionTemplate
+		err = store.GetTransactionTemplatesForProject(id, &ttpl)
+		if err != nil {
+			// TODO: handle error
+		}
+		// TODO: convert ttpl []*model.TransactionTemplate to sqlttpl []*sqlmodel.TransactionTemplate
+		//sqlttpl := migrateTransactionTemplate(ttpl)
+
+		// TODO: stpl migration
+		var stpl []*model.ScriptTemplate
+		store.GetScriptTemplatesForProject(id, &stpl)
+
+		// Store sqlProj in SQL db
+		err := sqldb.CreateProject(sqlProj, sqlttpl, stpl)
+	}
+
+}
+
+func migrateTransactionTemplate(ttpl *[]*model.TransactionTemplate) *[]*sqlModel.TransactionTemplate {
+	var sqlttpl []*sqlModel.TransactionTemplate
+
+	for i, ttp := range *ttpl {
+		sqlttp := sqlModel.TransactionTemplate{}
+		sqlttp.ID = ttp.ID
+		sqlttp.ProjectID = ttp.ID
+		sqlttp.Title = ttp.Title
+		sqlttp.Index = ttp.Index
+		sqlttp.Script = ttp.Script
+		sqlttpl = append(sqlttpl, &sqlttp)
+	}
+
+	return &sqlttpl
+}
+
+func migrateAccount(acc *model.Account) *sqlModel.Account {
+
+}
+
+func migrateProject(proj *model.InternalProject) *sqlModel.Project {
+	sqlProj := &sqlModel.Project{}
+	sqlProj.ID = proj.ID
+	sqlProj.UserID = proj.UserID
+	sqlProj.Secret = proj.Secret
+	sqlProj.PublicID = proj.PublicID
+	sqlProj.ParentID = proj.ParentID
+	sqlProj.Title = proj.Title
+	sqlProj.Description = proj.Description
+	sqlProj.Readme = proj.Readme
+	sqlProj.Seed = proj.Seed
+	sqlProj.TransactionCount = proj.TransactionCount
+	sqlProj.TransactionExecutionCount = proj.TransactionExecutionCount
+	sqlProj.TransactionExecutionCount = proj.TransactionExecutionCount
+	sqlProj.TransactionTemplateCount = proj.TransactionTemplateCount
+	sqlProj.ScriptTemplateCount = proj.ScriptTemplateCount
+	sqlProj.Persist = proj.Persist
+	sqlProj.CreatedAt = proj.CreatedAt
+	sqlProj.UpdatedAt = proj.UpdatedAt
+	sqlProj.Version = proj.Version
+	return sqlProj
 }
--- model/project.go
+++ model/project.go
@@ -19,8 +19,6 @@
 package model
 
 import (
-	"cloud.google.com/go/datastore"
-	"github.com/pkg/errors"
 	"time"
 
 	"github.com/Masterminds/semver"
@@ -84,83 +82,4 @@ func (p *Project) ExportPublicImmutable() *Project {
 		Version:     p.Version,
 		Mutable:     false,
 	}
-}
-
-// Load is used for datastore to SQL migration
-func (p *Project) Load(ps []datastore.Property) error {
-	tmp := struct {
-		ID                        string
-		UserID                    string
-		Secret                    string
-		PublicID                  string
-		ParentID                  *string
-		Title                     string
-		Description               string
-		Readme                    string
-		Seed                      int
-		TransactionCount          int
-		TransactionExecutionCount int
-		TransactionTemplateCount  int
-		ScriptTemplateCount       int
-		Persist                   bool
-		CreatedAt                 time.Time
-		UpdatedAt                 time.Time
-		Version                   *string
-	}{}
-
-	if err := datastore.LoadStruct(&tmp, ps); err != nil {
-		return err
-	}
-
-	if err := p.ID.UnmarshalText([]byte(tmp.ID)); err != nil {
-		return errors.Wrap(err, "failed to decode UUID")
-	}
-
-	if tmp.UserID != "" {
-		if err := p.UserID.UnmarshalText([]byte(tmp.UserID)); err != nil {
-			return errors.Wrap(err, "failed to decode UUID")
-		}
-	}
-
-	if tmp.Secret != "" {
-		if err := p.Secret.UnmarshalText([]byte(tmp.Secret)); err != nil {
-			return errors.Wrap(err, "failed to decode UUID")
-		}
-	}
-
-	if err := p.PublicID.UnmarshalText([]byte(tmp.PublicID)); err != nil {
-		return errors.Wrap(err, "failed to decode UUID")
-	}
-
-	if tmp.ParentID != nil && len(*tmp.ParentID) != 0 {
-		p.ParentID = new(uuid.UUID)
-		if err := p.ParentID.UnmarshalText([]byte(*tmp.ParentID)); err != nil {
-			return errors.Wrap(err, "failed to decode UUID")
-		}
-	} else {
-		p.ParentID = nil
-	}
-
-	if tmp.Version != nil && len(*tmp.Version) != 0 {
-		var err error
-		p.Version, err = semver.NewVersion(*tmp.Version)
-		if err != nil {
-			return errors.Wrap(err, "failed to parse project version")
-		}
-	}
-
-	p.Title = tmp.Title
-	p.Description = tmp.Description
-	p.Readme = tmp.Readme
-	p.Seed = tmp.Seed
-	p.TransactionCount = tmp.TransactionCount
-	p.TransactionExecutionCount = tmp.TransactionExecutionCount
-	p.TransactionTemplateCount = tmp.TransactionTemplateCount
-	p.ScriptTemplateCount = tmp.ScriptTemplateCount
-	p.Persist = tmp.Persist
-
-	p.CreatedAt = tmp.CreatedAt
-	p.UpdatedAt = tmp.UpdatedAt
-
-	return nil
 }
