971f5dbfebffe1dfcd06750e5ed68f3f1af64f81
Dylan Tinianov <dylantinianov@gmail.com>
2022-10-11 15:33:58 -0400

    Testing/add replica testing (#95)
    * Add replica testing
    
    * Found potential cached emulator issue
    
    * Update replica testing
    
    * Update dockerfile
    
    * Create replica tests
    
    * Undo dockerfile change

--- playground_test.go
+++ playground_test.go
@@ -458,6 +458,120 @@ type DeleteScriptTemplateResponse struct {
 
 const initAccounts = 5
 
+func TestReplicas(t *testing.T) {
+	// Each replica is a different client calling the API, but also an instance of the resolver
+	const numReplicas = 5
+
+	// Create replicas
+	var replicas []*Client
+	for i := 0; i < numReplicas; i++ {
+		replicas = append(replicas, newClient())
+	}
+
+	replicaIdx := 0 // current replica
+	// loadBalancer cycles through replicas
+	var loadBalancer = func() *Client {
+		replicaIdx = (replicaIdx + 1) % len(replicas)
+		return replicas[replicaIdx]
+	}
+
+	// Create project for all replica tests
+	c := loadBalancer()
+	project := createProject(t, c)
+	cookie := c.SessionCookie() // Use one session cookie for everything currently
+
+	t.Run("Execute transactions on multiple replicas", func(t *testing.T) {
+		for i := 0; i < 10; i++ {
+			const script = "transaction { execute { log(\"Hello, World!\") } }"
+			var resp CreateTransactionExecutionResponse
+			err := loadBalancer().Post(
+				MutationCreateTransactionExecution,
+				&resp,
+				client.Var("projectId", project.ID),
+				client.Var("script", script),
+				client.AddCookie(cookie),
+			)
+			require.NoError(t, err)
+			assert.Empty(t, resp.CreateTransactionExecution.Errors)
+			assert.Contains(t, resp.CreateTransactionExecution.Logs, "\"Hello, World!\"")
+			assert.Equal(t, script, resp.CreateTransactionExecution.Script)
+		}
+	})
+
+	t.Run("Re-deploy contracts on multiple replicas to initial accounts", func(t *testing.T) {
+		var contract = "pub contract Foo {}"
+
+		for i := 0; i < 10; i++ {
+			accountIdx := i % len(project.Accounts)
+			account := project.Accounts[accountIdx]
+
+			var updateResp UpdateAccountResponse
+			err := loadBalancer().Post(
+				MutationUpdateAccountDeployedCode,
+				&updateResp,
+				client.Var("projectId", project.ID),
+				client.Var("accountId", account.ID),
+				client.Var("code", contract),
+				client.AddCookie(cookie),
+			)
+			require.NoError(t, err)
+			assert.Equal(t, contract, updateResp.UpdateAccount.DeployedCode)
+			assert.Equal(t, updateResp.UpdateAccount.DeployedCode, contract)
+		}
+	})
+
+	/* TODO: This test must pass with cache invalidation fix
+	t.Run("Deploy new contracts to the same account on multiple replicas", func(t *testing.T) {
+		var accountsDeployedCode []string
+		for i := 0; i < len(project.Accounts); i++ {
+			accountsDeployedCode = append(accountsDeployedCode, "")
+		}
+
+		for i := 0; i < 10; i++ {
+			c := loadBalancer()
+			fmt.Println("Using replica", replicaIdx)
+			// Get next account in cycle
+			accountIdx := i % len(project.Accounts)
+			account := project.Accounts[accountIdx]
+
+			prevDeployedCode := accountsDeployedCode[accountIdx]
+			fmt.Println("Prev deployed for account 0: " + prevDeployedCode)
+
+			var respA GetAccountResponse
+			err := c.Post(
+				QueryGetAccount,
+				&respA,
+				client.Var("projectId", project.ID),
+				client.Var("accountId", account.ID),
+			)
+			require.NoError(t, err)
+			require.Equal(t, prevDeployedCode, respA.Account.DeployedCode)
+
+			contractNumber := strconv.Itoa(i)
+			var contract = "pub contract Foo" + contractNumber + " {}"
+
+			var respB UpdateAccountResponse
+			err = c.Post(
+				MutationUpdateAccountDeployedCode,
+				&respB,
+				client.Var("projectId", project.ID),
+				client.Var("accountId", account.ID),
+				client.Var("code", contract),
+				client.AddCookie(cookie),
+			)
+			require.NoError(t, err)
+
+			// Update deployed code for current account
+			accountsDeployedCode[accountIdx] = contract
+			fmt.Println("New deployed code for account 0: " + accountsDeployedCode[accountIdx])
+
+			assert.Equal(t, contract, respB.UpdateAccount.DeployedCode)
+			assert.Contains(t, respB.UpdateAccount.DeployedContracts, "Foo"+contractNumber)
+		}
+	})
+	*/
+}
+
 func TestProjects(t *testing.T) {
 	t.Parallel()
 
