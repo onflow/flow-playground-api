acda73942417178f98d05defe97ce9589501b4a6
Dylan Tinianov <dylantinianov@gmail.com>
2022-10-14 11:22:33 -0400

    Add error handling to emulatorCache

--- blockchain/cache.go
+++ blockchain/cache.go
@@ -20,24 +20,42 @@ package blockchain
 
 import (
 	"fmt"
+	"github.com/getsentry/sentry-go"
 	"github.com/google/uuid"
 	lru "github.com/hashicorp/golang-lru"
 )
 
 // newEmulatorCache returns a new instance of cache with provided capacity.
 func newEmulatorCache(capacity int) *emulatorCache {
-	OnEvicted := func(key interface{}, value interface{}) {
-		fmt.Printf("Cache evicted emulator for project: %s - (%v)\n", key.(uuid.UUID).String(), key)
+	emCache := &emulatorCache{
+		capacity: capacity,
+		cache:    nil,
+	}
+	_ = emCache.initializeCache()
+	return emCache
+}
+
+// setCache sets emulatorCache to a new lru cache and returns true if successful
+func (c *emulatorCache) initializeCache() bool {
+	var onEvicted = func(key interface{}, value interface{}) {
+		fmt.Printf("Cache evicted emulator for project: %s - (%v)\n",
+			key.(uuid.UUID).String(), key)
 	}
 
-	c, err := lru.NewWithEvict(capacity, OnEvicted)
+	cache, err := lru.NewWithEvict(c.capacity, onEvicted)
 	if err != nil {
-		panic(err)
+		c.cache = nil
+		sentry.CaptureException(err)
+		return false
 	}
 
-	return &emulatorCache{
-		cache: c,
-	}
+	c.cache = cache
+	return true
+}
+
+// checkCache return true if cache is accessible, or we can reset it successfully
+func (c *emulatorCache) checkCache() bool {
+	return c.cache != nil || c.initializeCache()
 }
 
 // emulatorCache caches the emulator state.
@@ -49,16 +67,23 @@ func newEmulatorCache(capacity int) *emulatorCache {
 //   doesn't receive that request so on next run it receives 0 executions but cached emulator contains state from previous
 //   executions that wasn't cleared
 type emulatorCache struct {
-	cache *lru.Cache
+	capacity int
+	cache    *lru.Cache
 }
 
 // reset the cache for the ID.
 func (c *emulatorCache) reset(ID uuid.UUID) {
+	if !c.checkCache() {
+		return
+	}
 	c.cache.Remove(ID)
 }
 
 // get returns a cached emulator if exists, but also checks if it's stale.
 func (c *emulatorCache) get(ID uuid.UUID) *emulator {
+	if !c.checkCache() {
+		return nil
+	}
 	val, ok := c.cache.Get(ID)
 	if !ok {
 		return nil
@@ -69,5 +94,8 @@ func (c *emulatorCache) get(ID uuid.UUID) *emulator {
 
 // add new entry in the cache.
 func (c *emulatorCache) add(ID uuid.UUID, emulator *emulator) {
+	if !c.checkCache() {
+		return
+	}
 	c.cache.Add(ID, emulator)
 }
