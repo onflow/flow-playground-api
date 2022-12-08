97160ad2519cd2dfcb9980a958a268530b1d1578
Dylan Tinianov <dylantinianov@gmail.com>
2022-12-08 13:06:56 -0500

    Add open telemetry for metrics (#147)
    * added otel tracing
    
    * add basic metrics
    
    * add provider, stdout for now
    
    * add metrics and trace to server
    
    * add telem handler
    
    * add todo
    
    * add middleware
    
    * add telemetry register
    
    * Add stale project metrics
    
    * Fix metrics handler
    
    * Add license headers
    
    * Create telemetry config
    
    * tidy
    
    * Stale project endpoint
    
    * Stale project endpoint
    
    * Query stale projects internally
    
    * remove print
    
    Co-authored-by: Gregor Gololicic <gregor.gololicic@dapperlabs.com>

---
+++ server/config/telemetry.go
@@ -0,0 +1,35 @@
+/*
+ * Flow Playground
+ *
+ * Copyright 2019 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+// TelemetryConfig defines tracing configuration
+type TelemetryConfig struct {
+	// StaleProjectQueryTime how often to query database for stale project count
+	StaleProjectQueryTime string `default:"0 0 * * *"`
+	// TracingEnabled determines whether to collect and export traces
+	TracingEnabled bool `default:"false"`
+	// TracingCollectorEndpoint is the OTEL collector endpoint to which traces should be sent
+	TracingCollectorEndpoint string
+}
+
+var _ configGetter = &TelemetryConfig{}
+
+func (c *TelemetryConfig) getConfig() {
+	getEnv("TELEMETRY", c)
+}
---
+++ telemetry/stales.go
@@ -0,0 +1,77 @@
+/*
+ * Flow Playground
+ *
+ * Copyright 2019 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package telemetry
+
+import (
+	"github.com/dapperlabs/flow-playground-api/model"
+	"github.com/dapperlabs/flow-playground-api/server/config"
+	"github.com/getsentry/sentry-go"
+	"github.com/pkg/errors"
+	"github.com/robfig/cron"
+	"time"
+)
+
+// staleDuration is the amount a time before a project is considered stale if not accessed
+var staleDuration = (time.Hour * 24) * time.Duration(config.Playground().StaleProjectDays)
+
+// staleProjectScanner scans for stale projects in the database
+var staleProjectScanner func(stale time.Duration, projs *[]*model.Project) error = nil
+
+func SetStaleProjectScanner(scanner func(stale time.Duration, projs *[]*model.Project) error) {
+	staleProjectScanner = scanner
+}
+
+// StaleProjectCounter returns the number of stale projects
+func StaleProjectCounter() (int, error) {
+	if staleProjectScanner == nil {
+		return 0, errors.New("stale project scanner not set")
+	}
+
+	var stales []*model.Project
+	err := staleProjectScanner(staleDuration, &stales)
+	if err != nil {
+		return 0, err
+	}
+
+	return len(stales), nil
+}
+
+// registerStaleProjectJob registers recurring query for stale project count
+func registerStaleProjectJob() error {
+	job := cron.New()
+
+	err := job.AddFunc(
+		config.Telemetry().StaleProjectQueryTime,
+		func() {
+			staleCount, err := StaleProjectCounter()
+			if err != nil {
+				sentry.CaptureException(err)
+				return
+			}
+			staleProjectGauge.Set(float64(staleCount))
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	job.Start()
+
+	return nil
+}
