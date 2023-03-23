cb6e4c084e5344bbb2080b97d4ff122fcb7c9c83
Dylan Tinianov <dylantinianov@gmail.com>
2023-03-23 12:06:50 -0400

    Create initial accounts for Migrated Projects (#224)
    * Create initial accounts
    
    * Create initial accounts

--- controller/projects.go
+++ controller/projects.go
@@ -151,6 +151,14 @@ func (p *Projects) Get(id uuid.UUID) (*model.Project, error) {
 		return nil, errors.Wrap(err, "failed to get project")
 	}
 
+	// Migrated projects require initial accounts to be created
+	if proj.TransactionExecutionCount == 0 {
+		_, err = p.blockchain.CreateInitialAccounts(proj.ID)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return &proj, nil
 }
 
