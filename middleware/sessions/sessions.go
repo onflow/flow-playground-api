49d91434379034aa115b7d42f809024aa484c983
Dylan Tinianov <dylantinianov@gmail.com>
2023-08-08 12:08:12 -0400

    Log sessions middleware

--- middleware/sessions/sessions.go
+++ middleware/sessions/sessions.go
@@ -40,6 +40,12 @@ func Middleware(store sessions.Store) func(http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := context.WithValue(r.Context(), sessionCtxKeySession, store)
 
+			fmt.Println("Sessions Middleware Handler:")
+			session, err := store.Get(r, "flow-playground")
+			if err != nil {
+				fmt.Println("    error getting flow-playground session:", err.Error())
+			}
+			fmt.Println("    Session Values:", session.Values)
 			r = r.WithContext(ctx)
 
 			next.ServeHTTP(w, r)
@@ -57,7 +63,7 @@ func Get(ctx context.Context, name string) *sessions.Session {
 		fmt.Println("invalid cookie:", err.Error())
 	}
 
-	// TODO: No cached session is present causing no userIDKey to be there
+	// TODO: No cached session is present causing no userIDKey to be there??
 
 	return session
 }
